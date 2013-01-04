9929417cdc4d1a1c7550c23efda7de47ac2828ba
apro <apro@aproMac.local>
2013-01-04 04:13:51 +0200

    Initial Commit

---
+++ Hellowee/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Hellowee
+//
+//  Created by apro on 04.01.13.
+//  Copyright (c) 2013 apro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
